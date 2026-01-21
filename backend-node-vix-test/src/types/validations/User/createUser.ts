924de6df5efcbda2bd9c8b45687b75a1696ac47d
Felipe Raulino <felipe.raaulino29@gmail.com>
2026-01-21 15:59:56 -0300

    feat: give more specifc password rules and descriptive zod error messages

--- backend-node-vix-test/src/types/validations/User/createUser.ts
+++ backend-node-vix-test/src/types/validations/User/createUser.ts
@@ -1,9 +1,43 @@
 import { z } from "zod";
 
 export const userCreatedSchema = z.object({
-  username: z.string().min(1, "Username is required"),
-  password: z.string().min(8, "Password must be at least 8 characters long"), // Make safe password,
-  email: z.string().email("Invalid email"),
+  username: z
+    .string({ required_error: "Username is required" })
+    .min(1, "Username is required"),
+  password: z
+    .string({ required_error: "Password is required" })
+    .superRefine((val, ctx) => {
+      if (val.length < 12)
+        ctx.addIssue({
+          code: "custom",
+          message: "Password must be at least 12 characters long",
+        });
+      if ((val.match(/\d/g) || []).length < 2)
+        ctx.addIssue({
+          code: "custom",
+          message: "Password must contain at least 2 numbers",
+        });
+      if ((val.match(/[a-z]/g) || []).length < 2)
+        ctx.addIssue({
+          code: "custom",
+          message: "Password must contain at least 2 lowercase letters",
+        });
+
+      if ((val.match(/[A-Z]/g) || []).length < 2)
+        ctx.addIssue({
+          code: "custom",
+          message: "Password must contain at least 2 uppercase letters",
+        });
+
+      if ((val.match(/[^a-zA-Z0-9]/g) || []).length < 2)
+        ctx.addIssue({
+          code: "custom",
+          message: "Password must contain at least 2 special characters",
+        });
+    }),
+  email: z
+    .string({ required_error: "Email is required" })
+    .email("Invalid email"),
   profileImgUrl: z.string().nullable().optional(),
   role: z.enum(["admin", "member", "manager"]).optional(),
   idBrandMaster: z.number().optional(),
