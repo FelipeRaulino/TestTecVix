edba14eee9a1fdb8609697c7a3fff580343a4924
Felipe Raulino <felipe.raaulino29@gmail.com>
2026-01-22 10:01:38 -0300

    feat: insert listAll method to user model

--- backend-node-vix-test/src/models/UserModel.ts
+++ backend-node-vix-test/src/models/UserModel.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../database/client";
+import { IListAll } from "../types/IListAll";
 import { TUserCreated } from "../types/validations/User/createUser";
 import { TUserUpdated } from "../types/validations/User/updateUser";
 
@@ -23,6 +24,60 @@ export class UserModel {
         email,
       },
     });
+  }
+
+  async totalCount({ query }: IListAll) {
+    const { idBrandMaster } = query;
+
+    return prisma.user.count({
+      where: {
+        deletedAt: null,
+        idBrandMaster: !idBrandMaster ? undefined : Number(idBrandMaster),
+        username: {
+          contains: query.search,
+        },
+      },
+    });
+  }
+
+  async listAll({ query }: IListAll) {
+    const limit = query.limit || 0;
+    const skip = query.page ? query.page * limit : query.offset || 0;
+    const { idBrandMaster } = query;
+    const orderBy =
+      query.orderBy?.map(({ field, direction }) => ({
+        [field]: direction,
+      })) || [];
+
+    const users = await prisma.user.findMany({
+      where: {
+        deletedAt: null,
+        idBrandMaster: !idBrandMaster ? undefined : Number(idBrandMaster),
+        username: {
+          contains: query.search,
+        },
+      },
+      skip,
+      take: limit || undefined,
+      orderBy: orderBy.length
+        ? orderBy
+        : {
+            updatedAt: "desc",
+          },
+      include: {
+        brandMaster: {
+          select: {
+            brandName: true,
+            brandLogo: true,
+          },
+        },
+      },
+    });
+
+    const totalCount = await this.totalCount({
+      query,
+    });
+    return { totalCount, result: users };
   }
 
   async createNewUser(data: TUserCreated) {
