d5221e4be3c0a39c7517c965659f4371ca22d03f
Felipe Raulino <felipe.raaulino29@gmail.com>
2026-01-22 08:57:52 -0300

    feat: add method to update user in controller layer

--- backend-node-vix-test/src/controllers/UserController.ts
+++ backend-node-vix-test/src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { UserService } from "../services/UserService";
 import { CustomRequest } from "../types/custom";
 import { STATUS_CODE } from "../constants/statusCode";
+/* import { user } from "@prisma/client"; */
 
 export class UserController {
   constructor() {}
@@ -11,5 +12,15 @@ export class UserController {
   async createUser(req: CustomRequest<unknown>, res: Response) {
     const result = await this.userService.createNewUser(req.body);
     return res.status(STATUS_CODE.CREATED).json(result);
+  }
+
+  async updateUser(req: CustomRequest<unknown>, res: Response) {
+    const { idUser } = req.params;
+    /* const user = req.user as user; */
+    const result = await this.userService.updateUser(
+      idUser as string,
+      req.body,
+    );
+    return res.status(STATUS_CODE.OK).json(result);
   }
 }
