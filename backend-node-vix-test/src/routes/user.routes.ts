076c435b3e4cc7bd4f1ee1fb82bfc0e426822677
Felipe Raulino <felipe.raaulino29@gmail.com>
2026-01-22 15:26:40 -0300

    feat: protect user routes to specific users

--- backend-node-vix-test/src/routes/user.routes.ts
+++ backend-node-vix-test/src/routes/user.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { UserController } from "../controllers/UserController";
 import { API_VERSION, ROOT_PATH } from "../constants/basePathRoutes";
+import { isManagerOrIsAdmin } from "../auth/isManagerOrIsAdmin";
+import { authUser } from "../auth/authUser";
 
 const BASE_PATH = API_VERSION.V1 + ROOT_PATH.USER; //api/v1/user
 
@@ -23,15 +25,20 @@ userRoutes.get(
 
 // ========= POSTs =========
 
-userRoutes.post(BASE_PATH, async (req, res) => {
+userRoutes.post(BASE_PATH, authUser, isManagerOrIsAdmin, async (req, res) => {
   await userController.createUser(req, res);
 });
 
 // ======== PUTs =========
 
-userRoutes.put(`${BASE_PATH}/:idUser`, async (req, res) => {
-  await userController.updateUser(req, res);
-});
+userRoutes.put(
+  `${BASE_PATH}/:idUser`,
+  authUser,
+  isManagerOrIsAdmin,
+  async (req, res) => {
+    await userController.updateUser(req, res);
+  },
+);
 
 // ======== DELETEs ========
 userRoutes.delete(
