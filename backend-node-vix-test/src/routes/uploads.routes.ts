e30eef62ca3d4ec1bd771e0ae4fbbe233a42e30d
Felipe Raulino <felipe.raaulino29@gmail.com>
2026-01-28 15:04:06 -0300

    feat: implement endpoint for brand image assets

--- backend-node-vix-test/src/routes/uploads.routes.ts
+++ backend-node-vix-test/src/routes/uploads.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { API_VERSION, ROOT_PATH } from "../constants/basePathRoutes";
 import { BucketController } from "../controllers/BucketController";
 import { BucketLocalService } from "../services/BucketLocalService";
+import { authUser } from "../auth/authUser";
+import multer from "multer";
 
 const BASE_PATH = API_VERSION.V1 + ROOT_PATH.UPLOADS; // /api/v1/uploads
 
@@ -14,8 +16,19 @@ export const makeBucketController = () => {
 
 const uploadsController = makeBucketController();
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 uploadsRoutes.get(`${BASE_PATH}/:objectName`, async (req, res) => {
   await uploadsController.getFileInBucketByObjectName(req, res);
 });
+
+uploadsRoutes.post(
+  `${BASE_PATH}/file`,
+  authUser,
+  upload.single("file"),
+  async (req, res) => {
+    await uploadsController.uploadFile(req, res);
+  },
+);
 
 export { uploadsRoutes };
